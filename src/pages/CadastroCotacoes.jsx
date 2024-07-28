5c6d776f204ec6eb248c473e458d0fe7819ed50e
Jackson Silva <jacksonslv36@gmail.com>
2024-07-28 17:15:59 -0300

    Já era pra estar lá mas enfim

--- src/pages/CadastroCotacoes.jsx
+++ src/pages/CadastroCotacoes.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import {useContext, useEffect, useState} from "react";
 import { Button, DropDown, TextField } from "../components";
 import { FornecedoresContext, ProdutosContext } from "../context";
 
@@ -9,40 +9,72 @@ const CadastroCotacoes = () => {
 
     const [selectedProduto, setSelectedProduto] = useState({});
     const [selectedFornecedor, setSelectedFornecedor] = useState({});
-    const [idEmEdicao, setIdEmEdicao] = useState(0);
+    const [idEmEdicao, setIdEmEdicao] = useState("");
     const [editando, setEditando] = useState(false);
     const [cotacoes, setCotacoes] = useState([]);
 
+    useEffect(() => {
+        if (editando) {
+            gerarCotacoes();
+        }
+    }, [editando]);
+
+    useEffect(() => {
+        if (cotacoes.length > 0) {
+            setEditando(true);
+        } else if (editando) {
+            alert("Não há cotações para editar!");
+            setEditando(false);
+        }
+    }, [cotacoes]);
+
     function gerarCotacoes() {
         let lista = [];
-        if (selectedProduto.cotacoes.length > 0 && selectedFornecedor.cotacoes.length > 0) {
-            lista = selectedProduto.cotacoes.filter(cotacao => selectedFornecedor.cotacoes.some(c => c.id === cotacao.id));
-        } else if(selectedProduto.cotacoes.length > 0) {
-            lista = selectedProduto.cotacoes;
-        } else if(selectedFornecedor.cotacoes.length > 0) {
-            lista = selectedFornecedor.cotacoes;
+        const produtoCotacoes = selectedProduto.cotacoes?.length > 0 ? [...selectedProduto.cotacoes] : [];
+        const fornecedorCotacoes = selectedFornecedor.cotacoes?.length > 0 ? [...selectedFornecedor.cotacoes] : [];
+
+        if (produtoCotacoes.length > 0 && fornecedorCotacoes.length > 0) {
+            lista = produtoCotacoes.filter(cotacao => fornecedorCotacoes.some(c => c.id === cotacao.id));
+        } else if (produtoCotacoes.length > 0) {
+            lista = produtoCotacoes;
+        } else if (fornecedorCotacoes.length > 0) {
+            lista = fornecedorCotacoes;
         } else {
             lista = [];
         }
+
         setCotacoes(lista);
     }
 
     function handleSelectProduto(event) {
-        let selected = produtos.find(produto => produto.id === parseInt(event.target.value))[0];
+        let selected = produtos.find(produto => produto.id === parseInt(event.target.value));
         setSelectedProduto(selected);
-        setIdEmEdicao(0);
+        setIdEmEdicao("");
         setEditando(false);
     }
 
     function handleSelectFornecedor(event) {
-        let selected = fornecedores.find(fornecedor => fornecedor.id === parseInt(event.target.value))[0];
+        let selected = fornecedores.find(fornecedor => fornecedor.id === parseInt(event.target.value));
         setSelectedFornecedor(selected);
-        setIdEmEdicao(0);
+        setIdEmEdicao("");
         setEditando(false);
     }
 
     function handleSelectCotacao(event) {
-        setIdEmEdicao(parseInt(event.target.value));
+        const cotacaoId = event.target.value;
+        setIdEmEdicao(cotacaoId);
+
+        const selectedCotacao = cotacoes.find(c => c.id === cotacaoId);
+
+        if (selectedCotacao) {
+            let preco = document.getElementById("preco");
+            let data = document.getElementById("data");
+            let observacoes = document.getElementById("observacoes");
+
+            preco.value = selectedCotacao.preco;
+            data.value = selectedCotacao.data;
+            observacoes.value = selectedCotacao.observacoes;
+        }
     }
 
     function handleCadastrar(event) {
@@ -59,6 +91,7 @@ const CadastroCotacoes = () => {
 
             let novaCotacao = {
                 id: `${produto.id}-${fornecedor.id}-${Math.floor(Math.random() * 1000)}`,
+                nome: `${produto.nome} vendido por ${fornecedor.nome} - ${data.value}`,
                 produto: produto.id,
                 fornecedor: fornecedor.id,
                 preco: Number(preco.value),
@@ -93,6 +126,8 @@ const CadastroCotacoes = () => {
             });
 
             setFornecedores(updatedFornecedores);
+            setSelectedFornecedor(updatedFornecedor);
+            setSelectedProduto(updatedProduto);
             setProdutos(updatedProdutos);
             alert("Cotação cadastrada com sucesso!");
 
@@ -101,35 +136,49 @@ const CadastroCotacoes = () => {
             observacoes.value = "";
         } else {
             alert("Preencha todos os campos!");
+            console.log(produto, fornecedor, preco.value, data.value);
         }
     }
 
     function handleEditar(event) {
         event.preventDefault();
-
         gerarCotacoes();
-        cotacoes.length > 0 ? setEditando(true) : alert("Não há cotações para editar!");
     }
 
     function handleSalvar(event) {
         event.preventDefault();
 
-        let produto = selectedProduto;
-        let fornecedor = selectedFornecedor;
+        let selectedCotacao = cotacoes.find(c => c.id === idEmEdicao);
+        let produto = produtos.find(produto => produto.id === selectedCotacao.produto);
+        let fornecedor = fornecedores.find(fornecedor => fornecedor.id === selectedCotacao.produto);
+
         let preco = document.getElementById("preco");
         let data = document.getElementById("data");
         let observacoes = document.getElementById("observacoes");
 
         if (produto && fornecedor && preco.value && data.value) {
-            let updatedCotacoes = produto.cotacoes.map(cotacao => {
+
+            let updatedCotacao = {
+                id: selectedCotacao.id,
+                produto: selectedCotacao.produto,
+                fornecedor: selectedCotacao.fornecedor,
+                nome: `${produto.nome} vendido por ${fornecedor.nome} - ${data.value}`,
+                preco: Number(preco.value),
+                data: data.value,
+                observacoes: observacoes.value
+            }
+
+            let updatedProdutoCotacoes = produto.cotacoes.map(cotacao => {
                 if (cotacao.id === idEmEdicao) {
-                    return {
-                        ...cotacao,
-                        fornecedor: fornecedor,
-                        preco: Number(preco.value),
-                        data: data.value,
-                        observacoes: observacoes.value
-                    }
+                    return updatedCotacao;
+                } else {
+                    return cotacao;
+                }
+            });
+
+            let updatedFornecedorCotacoes = fornecedor.cotacoes.map(cotacao => {
+                if (cotacao.id === idEmEdicao) {
+                    return updatedCotacao;
                 } else {
                     return cotacao;
                 }
@@ -137,8 +186,13 @@ const CadastroCotacoes = () => {
 
             let updatedProduto = {
                 ...produto,
-                cotacoes: updatedCotacoes
+                cotacoes: updatedProdutoCotacoes
             };
+
+            let updatedFornecedor = {
+                ...fornecedor,
+                cotacoes: updatedFornecedorCotacoes
+            }
 
             let updatedProdutos = produtos.map(produto => {
                 if (produto.id === updatedProduto.id) {
@@ -148,6 +202,15 @@ const CadastroCotacoes = () => {
                 }
             });
 
+            let updatedFornecedores = fornecedores.map(fornecedor => {
+                if (fornecedor.id === updatedFornecedor.id) {
+                    return updatedFornecedor;
+                } else {
+                    return fornecedor;
+                }
+            })
+
+            setFornecedores(updatedFornecedores);
             setProdutos(updatedProdutos);
             alert("Cotação salva com sucesso!");
 
@@ -155,7 +218,7 @@ const CadastroCotacoes = () => {
             data.value = "";
             observacoes.value = "";
             setEditando(false);
-            setIdEmEdicao(0);
+            setIdEmEdicao("");
         } else {
             alert("Preencha todos os campos!");
         }
@@ -164,14 +227,22 @@ const CadastroCotacoes = () => {
     function handleExcluir(event) {
         event.preventDefault();
 
-        let produto = selectedProduto;
+        let selectedCotacao = cotacoes.find(c => c.id === idEmEdicao);
+        let produto = produtos.find(produto => produto.id === selectedCotacao.produto);
+        let fornecedor = fornecedores.find(fornecedor => fornecedor.id === selectedCotacao.produto);
 
-        if (produto) {
-            let updatedCotacoes = produto.cotacoes.filter(cotacao => cotacao.id !== idEmEdicao);
+        if (produto && fornecedor) {
+            let updatedProdutoCotacoes = produto.cotacoes.filter(cotacao => cotacao.id !== idEmEdicao);
+            let updatedFornecedorCotacoes = fornecedor.cotacoes.filter(cotacao => cotacao.id !== idEmEdicao);
 
             let updatedProduto = {
                 ...produto,
-                cotacoes: updatedCotacoes
+                cotacoes: updatedProdutoCotacoes
+            };
+
+            let updatedFornecedor = {
+                ...fornecedor,
+                cotacoes: updatedFornecedorCotacoes
             };
 
             let updatedProdutos = produtos.map(produto => {
@@ -182,11 +253,20 @@ const CadastroCotacoes = () => {
                 }
             });
 
+            let updatedFornecedores = fornecedores.map(fornecedor => {
+                if (fornecedor.id === updatedFornecedor.id) {
+                    return updatedFornecedor;
+                } else {
+                    return fornecedor;
+                }
+            });
+
             setProdutos(updatedProdutos);
+            setFornecedores(updatedFornecedores);
             alert("Cotação excluída com sucesso!");
 
             setEditando(false);
-            setIdEmEdicao(0);
+            setIdEmEdicao("");
         } else {
             alert("Selecione uma cotação!");
         }
@@ -196,7 +276,7 @@ const CadastroCotacoes = () => {
         <div className="cadastroCotacoes">
             <h1>Cadastro de Cotações</h1>
             <form>
-                {!editando &&
+                {editando === false &&
                     <div className="dropdownWrapper">
                         <DropDown id="produto" label="Produto" options={produtos} disabled="Selecione um produto" onChange={handleSelectProduto} />
                         <DropDown id="fornecedor" label="Fornecedor" options={fornecedores} disabled="Selecione um fornecedor" onChange={handleSelectFornecedor} />
@@ -205,7 +285,7 @@ const CadastroCotacoes = () => {
                 <TextField id="preco" inputType="number" placeholder="Preço" label="Preço" />
                 <TextField id="data" inputType="date" label="Data" />
                 <TextField id="observacoes" inputType="textarea" placeholder="Observações" label="Observações" />
-                {!editando ?
+                {editando === false ?
                     (
                         <div className="botoes">
                             <Button texto="Cadastrar" onClick={handleCadastrar} />
